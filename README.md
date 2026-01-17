85abc0ca0d7909f4325b25dfc24d86288056ec59
1e1001 <michael@malinov.com>
2026-01-16 17:33:04 -0800

    webcam-based als

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ A fullscreen clock website.
 
 Currently using the legacy build until I make the new version.
 
+![Clock installation demo](./demo.jpg)
+
 ## Structure
-- `als-client`: ambient light sensor, [arduino esplora](https://arduino.cc/esplora) client.
-- `als-host`: ambient light sensor, linux host.
+- `als-camera`: ambient light sensor using the webcam, to dim the screen in low light
+- `als-client` and `als-host`: old als using an [arduino esplora](https://arduino.cc/esplora). kept for posterity
 - `gentz`: generate timezone abbreviations list (`tzabbr.js`)
 - `legacy.html`: legacy (uncustomizable) build, depends on `tzaddr.js`
 - `.`: clock web-app
---
+++ als-camera/src/main.rs
@@ -0,0 +1,130 @@
+#![expect(
+	clippy::print_stdout,
+	clippy::cast_precision_loss,
+	clippy::cast_sign_loss,
+	clippy::cast_possible_truncation,
+	reason = "binary"
+)]
+
+use std::fs::{File, read_to_string};
+use std::io::Write;
+use std::mem::replace;
+use std::path::PathBuf;
+use std::thread::sleep;
+use std::time::Duration;
+
+use clap::Parser;
+use rscam::{Camera, Config};
+
+/// webcam-based ambient light sensor.
+/// This does not send any camera controls (manual focus), so do that manually.
+#[derive(Debug, Parser)]
+#[command(version, long_about = None)]
+struct Args {
+	/// Video device name
+	video_path: String,
+	/// Prefix for backlight device
+	backlight_path: PathBuf,
+	/// Video format: [w]x[h]@[n]/[d]
+	video_format: String,
+	/// input value minimum
+	light_min: f32,
+	/// input value maximum
+	light_max: f32,
+	/// Black screen level 0..=1
+	screen_min: f32,
+	/// Screen curve exponent
+	screen_power: f32,
+}
+
+#[inline(never)]
+fn frame_sum(frame: &[u8]) -> u32 {
+	// only get the two Y's from YUYV video
+	frame
+		.as_chunks::<2>()
+		.0
+		.iter()
+		.map(|&[y, _]| u32::from(y))
+		.sum::<u32>()
+}
+
+const SECOND: Duration = Duration::from_secs(1);
+
+fn main() {
+	let Args {
+		video_path,
+		video_format,
+		backlight_path,
+		light_min,
+		light_max,
+		screen_min,
+		screen_power,
+	} = Args::parse();
+	let screen_max = read_to_string(backlight_path.join("max_brightness"))
+		.expect("failed to read max_brightness")
+		.trim()
+		.parse::<f32>()
+		.expect("bad max_brightness");
+	let screen_min = screen_min * screen_max;
+	let mut backlight = File::options()
+		.append(true)
+		.open(backlight_path.join("brightness"))
+		.expect("failed to open brightness");
+	let mut camera = Camera::new(&video_path).unwrap();
+	let (vf_resolution, vf_interval) = video_format.split_once('@').expect("vf bad parts");
+	let (resolution_w, resolution_h) = vf_resolution.split_once('x').expect("vf bad resolution");
+	let (interval_n, interval_d) = vf_interval.split_once('/').expect("vf bad interval");
+	let resolution = (
+		resolution_w.parse().expect("vf bad width"),
+		resolution_h.parse().expect("vf bad height"),
+	);
+	camera
+		.start(&Config {
+			interval: (
+				interval_n.parse().expect("vf bad width"),
+				interval_d.parse().expect("vf bad height"),
+			),
+			resolution,
+			format: b"YUYV",
+			..Default::default()
+		})
+		.unwrap();
+	let max_sum = (resolution.0 * resolution.1 * 255) as f32;
+	println!("max backlight {screen_max:?}, max sum {max_sum:?}");
+	let mut prev_time = 0;
+	let mut prev_status = false;
+	loop {
+		let frame = match camera.capture() {
+			Ok(frame) => frame,
+			Err(err) => {
+				println!("Read error: {err}");
+				prev_status = false;
+				sleep(SECOND);
+				continue;
+			}
+		};
+		let time = frame.get_timestamp();
+		// if the frame ever has more than 2²⁴ pixels, this won't work
+		let sum = frame_sum(&frame);
+		let light = sum as f32 / max_sum;
+		let dt = time - replace(&mut prev_time, time);
+		let mapped_light = (((light.clamp(light_min, light_max) - light_min)
+			/ (light_max - light_min))
+			.powf(screen_power)
+			* (screen_max - screen_min)
+			+ screen_min)
+			.round() as usize;
+		if let Err(err) = write!(backlight, "{mapped_light}") {
+			println!("Write error: {err}");
+			sleep(SECOND);
+			continue;
+		}
+		#[expect(clippy::cast_precision_loss, reason = "output")]
+		let mapped_pct = (mapped_light as f32) / screen_max;
+		if prev_status {
+			print!("\x1b[A\x1b[K");
+		}
+		println!("{dt:6}µs, raw={light:.4}, mapped={mapped_pct:.3} ({mapped_light}/{screen_max})");
+		prev_status = true;
+	}
+}
